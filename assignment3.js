1f0aa411109eae5f29cd78429da482e6c563afc0
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-05-05 09:32:10 -0400

    added greeting

--- assignment3.js
+++ assignment3.js
@@ -16,8 +16,15 @@ Greeter2.prototype.greet = function(name){
 }
 
 
-
-
+function Greeter3(name){
+}
+Greeter3.prototype.greet = function(name){
+  this.name = name
+  console.log(this.greeting + " " + name + "!");
+}
+Greeter3.prototype.setGreeting = function(greeting){
+  this.greeting = greeting
+}
 
 
 var greeter1 = new Greeter1(); //constructor
@@ -27,7 +34,16 @@ if (greeter1.__proto__ === Greeter1.prototype){
 }
 greeter1.greet();
 
+console.log("********");
 
 var greeter2 = new Greeter2();
 greeter2.greet("Don");
 greeter2.greet("Joe");
+
+console.log("********");
+
+var greeter3 = new Greeter3();
+greeter3.setGreeting("Hi");
+greeter3.greet("Don");
+greeter3.setGreeting("Hello");
+greeter3.greet("Joe");
