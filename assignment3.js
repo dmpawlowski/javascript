8913f5dc6e4406b49fdbe1d901febaa6270f11ae
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-05-05 07:56:07 -0400

    added comments

--- assignment3.js
+++ assignment3.js
@@ -1,3 +1,4 @@
+//create class
 function Greeter(){
   Greeter.prototype.greet = function(){
     console.log("Hello World!");
@@ -6,7 +7,8 @@ function Greeter(){
 
 
 
-var greeter = new Greeter();
+var greeter = new Greeter(); //constructor
+//check to see if class is built properly
 if (greeter.__proto__ === Greeter.prototype){
   console.log("Hooray");
 }
