fba6ebba153b2eb74dc97c29f45ae83ca0d9a2fb
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-06-08 19:17:37 -0400

    almost finished with graying out item

--- todo/appl.js
+++ todo/appl.js
@@ -6,10 +6,30 @@ function AddToDo(){
 function CreateToDo(){
   var input = document.getElementsByClassName('input');
   var li = document.createElement('li');
+  var p = document.createElement('p');
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = 'checkbox';
   var content = document.createTextNode(input[0].value);
-  li.appendChild(content);
+  li.appendChild(checkbox);
+  li.appendChild(p);
+  p.appendChild(content)
   var ul = document.getElementsByTagName('ul');
   ul[0].appendChild(li);
+  Checkbox();
+}
+
+function Checkbox(){
+  var cb = document.getElementById('checkbox');
+  if (cb){
+  cb.addEventListener('click', function(){
+    if (cb.checked == true){
+      console.log('yay');
+      var p = document.getElementsByTagName('p').className = "grayout";
+    }
+  });
+  }
+  
 }
 
 AddToDo();
