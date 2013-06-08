80ace250c5ac306a06eb19e5110ab68df9239ce6
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-06-08 16:05:29 -0400

    create li with input field text

--- todo/appl.js
+++ todo/appl.js
@@ -1,12 +1,13 @@
 function AddToDo(){
-  var input = document.getElementsByClassName('input');
   var button = document.getElementsByTagName('button');
   button[0].addEventListener('click', CreateToDo);
 }
 
 function CreateToDo(){
+  var input = document.getElementsByClassName('input');
   var li = document.createElement('li');
-  var content = document.createTextNode('yay');
+  var content = document.createTextNode(input[0].value);
+  li.appendChild(content);
   var ul = document.getElementsByTagName('ul');
   ul[0].appendChild(li);
 }
