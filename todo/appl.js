a8e7ece28edae9648fb1d95cac3d8dcc81939278
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-06-11 09:24:09 -0400

    cleaned up code

--- todo/appl.js
+++ todo/appl.js
@@ -1,44 +1,43 @@
-function AddToDo(){
-  var button = document.getElementsByTagName('button');
-  button[0].addEventListener('click', CreateToDo);
+function AddTask(){
+  var button = document.getElementById('button');
+  button.addEventListener('click', CreateTask);
 }
 
-function CreateToDo(){
-  var input = document.getElementsByClassName('input');
+function CreateTask(){
+  var input = document.getElementById('input');
   var li = document.createElement('li');
   var p = document.createElement('p');
   var checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.className = 'checkbox';
-  var content = document.createTextNode(input[0].value);
+  var content = document.createTextNode(input.value);
+  var ul = document.getElementById('list');
+
   li.appendChild(checkbox);
   li.appendChild(p);
   p.appendChild(content)
-  var ul = document.getElementsByTagName('ul');
-  ul[0].appendChild(li);
+  ul.appendChild(li);
   Checkbox();
 }
 
 function Checkbox(){
   var cb = document.getElementsByClassName('checkbox');
   for (i = 1; i <= cb.length; i ++){
-    cb[i-1].addEventListener('click', CompleteToDo);
+    cb[i-1].addEventListener('click', CompleteTask);
   }
 }
 
-function CompleteToDo(e){
+function CompleteTask(e){
   var cb = e.target;
   var node = cb.parentNode;
   var task = node.querySelector('p');
   if (task.className.match("grayout")){
-    console.log("matched");
     task.className = "";
   } 
   else{
     task.className = "grayout";
-    console.log("didnt match");
   }
 } 
 
 
-AddToDo();
+AddTask();
