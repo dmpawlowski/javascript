7189fa5613c52aa4096dfce6a37154695777cdad
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-06-29 12:28:03 -0400

    double clicking task opens text box

--- todo/appl.js
+++ todo/appl.js
@@ -16,6 +16,7 @@ function CreateTaskElements(){
   //how does the above line work? input isn't defined
   var inputTask = {
     task : taskString
+      //state (imcomplete vs complete) (css)
   };
   tasks.push(inputTask);
   var context = {
@@ -32,6 +33,11 @@ function CreateTaskElements(){
 
 function CheckboxListener(){
   var ulNode = document.getElementById('replace');
+  ulNode.addEventListener('dblclick', function(evt){
+    if (evt.target && evt.target.nodeName === "P"){
+      EditTask(evt);
+    }
+  });
   ulNode.addEventListener('click', function(evt){
     if (evt.target && evt.target.nodeName === "INPUT"){
       CompletedTaskStyle(evt);
@@ -65,7 +71,24 @@ function RestoreSession(){
   }
 }
 
+function EditTask(evt){
+  console.log('edit!');
+  var oldElement = evt.srcElement;
+  var oldText = evt.srcElement.innerHTML;
+  var input = document.createElement("input");
+  input.value = oldText;
+  input.type = "text";
+  console.log(oldElement);
+  console.log(oldElement.parentNode);
+  oldElement.parentNode.replaceChild(input, oldElement);
+}
 
 RestoreSession();
 AddTask();
 CheckboxListener();
+
+//use the task array as the data base
+//split create elements into two funciton
+//one that reads/writes to the database 
+//one that displays the database
+//json api
